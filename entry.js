3a2939ccb70ee9d60747baebdc421b8bb69a936d
Laurent Blondy <laurent.blondy1@gmail.com>
2020-01-22 12:59:06 +0100

    7

--- entry.js
+++ entry.js
@@ -16,4 +16,8 @@ function handleResize () {
 }
 window.addEventListener( "resize", handleResize )
 
+const weather_tablet_vid = document.querySelector( ".app-video__tablet" )
+const weather_phone_vid = document.querySelector( ".app-video__phone" )
 
+weather_tablet_vid.play()
+weather_tablet_vid.addEventListener( "ended", () => weather_phone_vid.play() )
