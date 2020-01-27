f210b67d84769618772463a49f27eea5d27fa19a
Laurent Blondy <laurent.blondy1@gmail.com>
2020-01-27 12:34:49 +0100

    heeeex

--- src/utils/handle_responsive_CSS_variables.js
+++ src/utils/handle_responsive_CSS_variables.js
@@ -1,13 +1,4 @@
 export default function handle_responsive_CSS_variables () {
 	const vh100 = Math.min( window.innerHeight, document.documentElement.clientHeight )
 	document.documentElement.style.setProperty( "--vh100", `${ vh100 }px` )
-
-	if ( window.matchMedia( "(max-width:1200px)" ).matches ) {
-		document.documentElement.style.setProperty( "--hex2-height", "5.92rem" )
-		document.documentElement.style.setProperty( "--hex1-width", "3.5rem" )
-	}
-	else if ( window.matchMedia( "(min-width:1200px)" ).matches ) {
-		document.documentElement.style.setProperty( "--hex2-height", "7rem" )
-		document.documentElement.style.setProperty( "--hex1-width", "3.5rem" )
-	}
 }
