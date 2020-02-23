69cfe13a25ba07b9a50d806ae073600915782870
Laurent Blondy <laurent.blondy1@gmail.com>
2020-02-23 21:12:48 +0100

    1

--- src/components/portfolio/Images.js
+++ src/components/portfolio/Images.js
@@ -4,18 +4,120 @@ import { setIntroClasses } from "./setIntroClasses"
 export function Images ( sources ) {
 	this.sources = sources
 	this.containers = [].slice.call( document.querySelectorAll( ".app-media-container" ) )
-	this.src = [ "weather-desktop.jpg", "weather-desktop.jpg", "weather-desktop.jpg" ]
+	this.src = [
+		[ "weather-desktop-1.jpg", "weather-desktop-2.jpg", "weather-desktop-3.jpg" ],
+		[ "weather-desktop-1.jpg", "weather-desktop-2.jpg", "weather-desktop-3.jpg" ],
+		[ "weather-desktop-1.jpg", "weather-desktop-2.jpg", "weather-desktop-3.jpg" ]
+	]
 	this.images = ""
 }
 Images.prototype.load = function () {
 	this.containers.forEach( ( container, i ) => {
-		container.innerHTML = `
-			<img class="app-image" src="${ this.sources[ this.src[ i ] ] }" alt="weather-app-pic" />`
+		const slides = this.src[ i ].reduce( ( r, c ) => [ ...r, this.sources[ c ] ], [] )
+
+		if ( "IntersectionObserver" in window && "Promise" in window )
+			container.innerHTML = `
+				<div class="app-slider">
+					<div class="app-image central">
+						<img data-src="${ slides[ 0 ] }" data-srcset="${ slides[ 0 ] }" alt="weather-app-pic" />
+					</div>
+					<div class="app-image right" >
+						<img data-src="${ slides[ 1 ] }" data-srcset="${ slides[ 1 ] }" alt="weather-app-pic" />
+					</div>
+					<div class="app-image left">
+						<img data-src="${ slides[ 2 ] }" data-srcset="${ slides[ 2 ] }" alt="weather-app-pic" />
+					</div>
+				</div>
+				<div class="app-slider-dots">
+					<div class="dot"></div>
+					<div class="dot"></div>
+					<div class="dot"></div>
+				</div>`
+		else {
+			container.innerHTML = `
+				<div class="app-slider">
+					<div>
+						<img class="app-image central" src="${ slides[ 0 ] }" srcset="${ slides[ 0 ] }" alt="weather-app-pic" />
+					</div>
+					<div>
+						<img class="app-image right" src="${ slides[ 1 ] }" srcset="${ slides[ 1 ] }" alt="weather-app-pic" />
+					</div>
+					<div>
+						<img class="app-image left" src="${ slides[ 2 ] }" srcset="${ slides[ 2 ] }" alt="weather-app-pic" />
+					</div>
+				</div>`
+		}
 	} )
-	this.images = [].slice.call( document.querySelectorAll( ".app-image" ) )
-	this.setOnVisible()
+	this.imagesCont = [].slice.call( document.querySelectorAll( ".app-image.central" ) )
+	if ( "IntersectionObserver" in window ) {
+		this.setOnVisible()
+		this.setLazyLoad()
+	}
 }
 Images.prototype.setOnVisible = function () {
-	onVisible( this.images, { class: "fadeFromBottom" } )
+	const that = this
+	const onVisibleObs = new IntersectionObserver( cb, { threshold: 0.01 } )
+	function cb ( entries ) {
+		entries.forEach( e => {
+			if ( e.intersectionRatio > 0 ) {
+				e.target.classList.remove( "hideRight" )
+				that.setAutoSlide( e.target )
+				onVisibleObs.unobserve( e.target )
+			}
+		} )
+	}
+	this.imagesCont.forEach( img => {
+		img.classList.add( "hideRight" )
+		onVisibleObs.observe( img )
+	} )
 	setIntroClasses()
+}
+Images.prototype.setAutoSlide = function ( center ) {
+	let cen = center
+	let right = cen.nextElementSibling
+	let left = right.nextElementSibling
+	let temp
+
+	setInterval( () => {
+		cen.className = "app-image"
+		cen.classList.add( "left" )
+		right.className = "app-image"
+		right.classList.add( "central" )
+		left.className = "app-image"
+		left.classList.add( "right" )
+
+		temp = left
+		left = cen
+		cen = right
+		right = temp
+	}, 5000 )
+}
+Images.prototype.setLazyLoad = function () {
+	this.lazyObs = new IntersectionObserver( cb, { threshold: 0.01, rootMargin: "0px 0px 500px 0px" } )
+	function cb ( entries ) {
+		entries.forEach( e => {
+			if ( e.intersectionRatio > 0 ) {
+				const promise = new Promise( function ( resolve, reject ) {
+					const img = e.target.children[ 0 ]
+					img.src = img.dataset.src
+					img.srcset = img.dataset.srcset
+
+					img.onload = () => resolve( e.target )
+					img.onerror = () => reject( new Error( "Could not load portfolio images" ) )
+				} )
+				promise.then( img => {
+					const next1 = img.nextElementSibling.children[ 0 ]
+					const next2 = img.nextElementSibling.nextElementSibling.children[ 0 ]
+					next1.src = next1.dataset.src
+					next1.srcset = next1.dataset.srcset
+					next2.src = next2.dataset.src
+					next2.srcset = next2.dataset.srcset
+				} )
+				this.unobserve( e.target )
+			}
+		} )
+	}
+	this.imagesCont.forEach( cont => {
+		this.lazyObs.observe( cont )
+	} )
 }
--- src/components/portfolio/Videos.js
+++ src/components/portfolio/Videos.js
@@ -12,17 +12,19 @@ export function Videos ( sources ) {
 Videos.prototype.load = function () {
 	this.containers.forEach( ( container, i ) => {
 		container.innerHTML = `
-		<video class="app-video__tablet ${i % 2 == 0 ? "hideLeft" : "hideRight" }" data-index="${ i }" muted="muted" controls="true">
+		<video class="app-video__tablet ${( "IntersectionObserver" in window ) && ( i % 2 == 0 ? "hideLeft" : "hideRight" ) }" data-index="${ i }" muted="muted" controls="true">
 			<source src="${ this.sources[ this.tablet_src[ i ] ] }" type="video/mp4" >
 		</video>
-		<video class="app-video__phone hideBottom" muted="muted" controls="true">
+		<video class="app-video__phone ${( "IntersectionObserver" in window ) && "hideBottom" }" muted="muted" controls="true">
 			<source src="${ this.sources[ this.phone_src[ i ] ] }" type="video/mp4">
 		</video>`
 	} )
 	this.tablets = [].slice.call( document.querySelectorAll( ".app-video__tablet" ) )
 	this.phones = [].slice.call( document.querySelectorAll( ".app-video__phone" ) )
-	this.setOnVisible()
-	this.setPlayPauseObs()
+	if ( "IntersectionObserver" in window ) {
+		this.setOnVisible()
+		this.setPlayPauseObs()
+	}
 	return this
 }
 
@@ -58,16 +60,15 @@ Videos.prototype.setPlayPauseObs = function () {
 
 			if ( e.intersectionRatio >= 1 ) {
 				if ( state === "tablet" ) {
-					try { tablet.play() }
-					catch ( e ) { tablet.play(); console.log( "try again" ) }
-					tablet.playbackRate = 15
+					tablet.play()
+					// tablet.playbackRate = 15
 					!tablet.classList.contains( "playing-tablet" ) && tablet.classList.add( "playing-tablet" )
 					!phone.classList.contains( "hiding-phone" ) && phone.classList.add( "hiding-phone" )
 				}
 				if ( state === "phone" ) {
-					try { phone.play() }
-					catch ( e ) { phone.play(); console.log( "try again" ) }
-					phone.playbackRate = 15
+					console.log( phone.readyState, phone )
+					phone.play()
+					// phone.playbackRate = 15
 					!phone.classList.contains( "playing-phone" ) && phone.classList.add( "playing-phone" )
 					!tablet.classList.contains( "hideLeft" ) && tablet.classList.add( "hideLeft" )
 				}
@@ -83,14 +84,17 @@ Videos.prototype.switchToPhone = function ( e ) {
 	const tablet = e.target
 	const phone = e.target.nextElementSibling
 
+	phone.parentElement.dataset.playing = "phone"
 	tablet.classList.remove( "playing-tablet" )
 	tablet.classList.add( "hideLeft" )
 	phone.classList.remove( "hiding-phone" )
 	phone.classList.add( "playing-phone" )
-	phone.play()
-	phone.playbackRate = 15
+	console.log( phone.readyState, phone )
+	phone.play().then( ( e ) => console.log( phone.paused ) )
+	// phone.playbackRate = 15
 	phone.addEventListener( "ended", function () {
 		tablet.classList.remove( "hideLeft" )
 		phone.classList.remove( "playing-phone" )
+		phone.parentElement.dataset.playing = "tablet"
 	}, { once: true } )
 }
