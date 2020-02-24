1c5e3f7dec3acf99fb326ab58048e89530ab04b6
Laurent Blondy <laurent.blondy1@gmail.com>
2020-02-24 08:11:00 +0100

    2

--- src/components/portfolio/Images.js
+++ src/components/portfolio/Images.js
@@ -18,21 +18,21 @@ Images.prototype.load = function () {
 		if ( "IntersectionObserver" in window && "Promise" in window )
 			container.innerHTML = `
 				<div class="app-slider">
-					<div class="app-image central">
-						<img data-src="${ slides[ 0 ] }" data-srcset="${ slides[ 0 ] }" alt="weather-app-pic" />
+					<div class="app-image active img1">
+						<img class="central" data-src="${ slides[ 0 ] }" data-srcset="${ slides[ 0 ] }" alt="weather-app-pic" />
 					</div>
-					<div class="app-image right" >
-						<img data-src="${ slides[ 1 ] }" data-srcset="${ slides[ 1 ] }" alt="weather-app-pic" />
+					<div class="app-image img2" >
+						<img class="right" data-src="${ slides[ 1 ] }" data-srcset="${ slides[ 1 ] }" alt="weather-app-pic" />
 					</div>
-					<div class="app-image left">
-						<img data-src="${ slides[ 2 ] }" data-srcset="${ slides[ 2 ] }" alt="weather-app-pic" />
+					<div class="app-image img3">
+						<img class="left" data-src="${ slides[ 2 ] }" data-srcset="${ slides[ 2 ] }" alt="weather-app-pic" />
 					</div>
 				</div>`
 		else {
 			container.innerHTML = `
 				<div class="app-slider">
 					<div>
-						<img class="app-image central" src="${ slides[ 0 ] }" srcset="${ slides[ 0 ] }" alt="weather-app-pic" />
+						<img class="app-image active" src="${ slides[ 0 ] }" srcset="${ slides[ 0 ] }" alt="weather-app-pic" />
 					</div>
 					<div>
 						<img class="app-image right" src="${ slides[ 1 ] }" srcset="${ slides[ 1 ] }" alt="weather-app-pic" />
@@ -43,50 +43,31 @@ Images.prototype.load = function () {
 				</div>`
 		}
 	} )
-	this.imagesCont = [].slice.call( document.querySelectorAll( ".app-image.central" ) )
+	this.imagesCont = [].slice.call( document.querySelectorAll( ".app-image.active" ) )
 	if ( "IntersectionObserver" in window ) {
 		this.setOnVisible()
 		this.setLazyLoad()
 	}
 }
 Images.prototype.setOnVisible = function () {
-	const that = this
 	const onVisibleObs = new IntersectionObserver( cb, { threshold: 0.01 } )
 	function cb ( entries ) {
 		entries.forEach( e => {
 			if ( e.intersectionRatio > 0 ) {
 				e.target.classList.remove( "hideRight" )
-				that.setAutoSlide( e.target )
 				onVisibleObs.unobserve( e.target )
 			}
 		} )
 	}
 	this.imagesCont.forEach( img => {
+		console.log( 65496798798 )
 		img.classList.add( "hideRight" )
 		onVisibleObs.observe( img )
+		new AutoSlide( img ).setAutoSlideObs()
 	} )
 	setIntroClasses()
 }
-Images.prototype.setAutoSlide = function ( center ) {
-	let cen = center
-	let right = cen.nextElementSibling
-	let left = right.nextElementSibling
-	let temp
 
-	setInterval( () => {
-		cen.className = "app-image"
-		cen.classList.add( "left" )
-		right.className = "app-image"
-		right.classList.add( "central" )
-		left.className = "app-image"
-		left.classList.add( "right" )
-
-		temp = left
-		left = cen
-		cen = right
-		right = temp
-	}, 5000 )
-}
 Images.prototype.setLazyLoad = function () {
 	this.lazyObs = new IntersectionObserver( cb, { threshold: 0.01, rootMargin: "0px 0px 500px 0px" } )
 	function cb ( entries ) {
@@ -115,4 +96,46 @@ Images.prototype.setLazyLoad = function () {
 	this.imagesCont.forEach( cont => {
 		this.lazyObs.observe( cont )
 	} )
+}
+
+function AutoSlide ( center ) {
+	this.center = center.children[ 0 ]
+	this.right = this.center.parentElement.nextElementSibling.children[ 0 ]
+	this.left = this.right.parentElement.nextElementSibling.children[ 0 ]
+	this.autoSlide = ""
+}
+AutoSlide.prototype.startAutoSlide = function () {
+	this.autoSlide = setInterval( autoSlideCB.bind( this ), 6500 )
+
+	function autoSlideCB () {
+		this.center.className = this.right.className = this.left.className = "app-image"
+		this.center.classList.add( "left" )
+		this.center.parentElement.classList.remove( "active" )
+		this.right.classList.add( "center" )
+		this.right.parentElement.classList.add( "active" )
+		this.left.classList.add( "right" )
+
+		const temp = this.center
+		this.center = this.right;
+		this.right = this.left;
+		this.left = temp
+	}
+}
+AutoSlide.prototype.stopAutoSlide = function () {
+	clearInterval( this.autoSlide )
+}
+AutoSlide.prototype.setAutoSlideObs = function () {
+	const that = this
+	this.obs = new IntersectionObserver( cb, { threshold: 0.9, rootMargin: "0px 500px 0px 500px" } )
+	function cb ( entries ) {
+		entries.forEach( e => {
+			if ( e.intersectionRatio >= 0.9 ) {
+				that.startAutoSlide()
+			}
+			else {
+				that.autoSlide && that.stopAutoSlide()
+			}
+		} )
+	}
+	this.obs.observe( this.center.parentElement )
 }
