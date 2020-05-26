8ebe55ef69846a35e32d3f7b6186cb60640b4554
Laurent Blondy <laurent.blondy1@gmail.com>
2020-05-26 02:56:01 +0200

    2

--- src/JS/showPhone.js
+++ src/JS/showPhone.js
@@ -1,12 +1,12 @@
-import { writeOnVisible } from "lb-write"
+import { writeOnVisible } from "lb-write";
 
-export function setup_showPhone () {
-	const target = document.querySelector( ".click-to-see" )
+export function setup_showPhone() {
+	const target = document.querySelector(".click-to-see");
 	const handleClick = () => {
-		target.className = "personal-data"
-		target.textContent = "342.517.3316"
-		writeOnVisible( target, { speed: 2 } )
-		target.removeEventListener( "click", handleClick )
-	}
-	target.addEventListener( "click", handleClick )
+		target.className = "personal-data";
+		target.textContent = "351.631.7880";
+		writeOnVisible(target, { speed: 2 });
+		target.removeEventListener("click", handleClick);
+	};
+	target.addEventListener("click", handleClick);
 }
