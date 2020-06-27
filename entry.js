4e9a962bd52aa68898f987e7c73777e5de284e33
Laurent Blondy <laurent.blondy1@gmail.com>
2020-06-27 11:36:59 +0200

    1

--- entry.js
+++ entry.js
@@ -1,57 +1,40 @@
-import "./src/SCSS/common.scss"
-import { setupNavBar } from "./src/JS/navbar.js"
-import { setupServices } from "./src/JS/services.js"
-import { setupDotAnim } from "./src/JS/dotAnim.js"
-import { ResizeHandler } from "./src/JS/portfolio/ResizeHandler.js"
-import { Videos } from "./src/JS/portfolio/Videos.js"
-import { Images } from "./src/JS/portfolio/Images.js"
-import { setup_showPhone } from "./src/JS/showPhone.js"
-import { fix_VH, fix_HashNav, getMediaSources } from "./src/utils"
+import "./src/SCSS/common.scss";
+import { setupNavBar } from "./src/JS/navbar.js";
+import { setupServices } from "./src/JS/services.js";
+import { setupDotAnim } from "./src/JS/dotAnim.js";
+import { ResizeHandler } from "./src/JS/portfolio/ResizeHandler.js";
+import { Videos } from "./src/JS/portfolio/Videos.js";
+import { Images } from "./src/JS/portfolio/Images.js";
+import { setup_showPhone } from "./src/JS/showPhone.js";
+import LazyLoader from 'lb-lazy-images';
+import { fix_VH, fix_HashNav, getMediaSources } from "./src/utils";
 
-let skills = document.querySelectorAll( ".profile-content__skills-skill span" )
-let myData = document.querySelectorAll( ".contact-content__intro--data .personal-data" )
+LazyLoader.loadOnScroll('.profile-bg__img', { rootMargin: '200px 200px 200px 200px' });
 
-setupNavBar()
-setupServices()
-setupDotAnim( skills, 2 )
-setupDotAnim( myData, 1 )
-setup_showPhone()
-fix_HashNav()
+let skills = document.querySelectorAll(".profile-content__skills-skill span");
+let myData = document.querySelectorAll(".contact-content__intro--data .personal-data");
 
-let sources = getMediaSources( require( "./src/assets/portfolio-media/*.*" ) )
-const videos = new Videos( sources )
-const images = new Images( sources )
-const resizeHandler = new ResizeHandler( { "video": () => videos.load(), "image": () => images.load() } )
+setupNavBar();
+setupServices();
+setupDotAnim(skills, 2);
+setupDotAnim(myData, 1);
+setup_showPhone();
+fix_HashNav();
 
-function handleResize () {
-	fix_VH()
-	if ( window.matchMedia( "(max-width:1199.8px)" ).matches ) {
-		resizeHandler.setState( "image" )
+let sources = getMediaSources(require("./src/assets/portfolio-media/*.*"));
+const videos = new Videos(sources);
+const images = new Images(sources);
+const resizeHandler = new ResizeHandler({ "video": () => videos.load(), "image": () => images.load() });
+
+function handleResize() {
+	fix_VH();
+	if (window.matchMedia("(max-width:1199.8px)").matches) {
+		resizeHandler.setState("image");
 	}
-	else if ( window.matchMedia( "(min-width:1200px)" ).matches ) {
-		resizeHandler.setState( "video" )
+	else if (window.matchMedia("(min-width:1200px)").matches) {
+		resizeHandler.setState("video");
 	}
 }
-handleResize()
-window.addEventListener( "resize", handleResize )
+handleResize();
+window.addEventListener("resize", handleResize);
 
-
-
-
-
-
-
-
-
-
-
-
-
-/*************** VIDEO *****************A/
-// const weather_tablet_vid = document.querySelector( ".app-video__tablet" )
-// const weather_phone_vid = document.querySelector( ".app-video__phone" )
-
-// weather_tablet_vid.play()
-// weather_tablet_vid.addEventListener( "ended", () => weather_phone_vid.play() )
-
-/*************** CONTACT FORM *****************/
--- src/JS/portfolio/Images.js
+++ src/JS/portfolio/Images.js
@@ -1,141 +1,140 @@
-import { onVisible } from "lb-onvisible"
-import { setIntroClasses } from "./setIntroClasses"
+import { setIntroClasses } from "./setIntroClasses";
 
-export function Images ( sources ) {
-	this.sources = sources
-	this.containers = [].slice.call( document.querySelectorAll( ".app-media-container" ) )
+export function Images(sources) {
+	this.sources = sources;
+	this.containers = [].slice.call(document.querySelectorAll(".app-media-container"));
 	this.src = [
 		[ "weather-desktop-1", "weather-desktop-2", "weather-desktop-3" ],
 		[ "weather-desktop-1", "weather-desktop-2", "weather-desktop-3" ],
 		[ "weather-desktop-1", "weather-desktop-2", "weather-desktop-3" ]
-	]
-	this.images = ""
+	];
+	this.images = "";
 }
 Images.prototype.load = function () {
-	this.containers.forEach( ( container, i ) => {
+	this.containers.forEach((container, i) => {
 
-		const slides = this.src[ i ].reduce( ( r, c ) => {
-			const realSrc = [ c + "_450w.jpg", c + "_800w.jpg", c + "_1200w.jpg" ].map( src => this.sources[ src ] )
-			return [ ...r, ...realSrc ]
-		}, [] )
+		const slides = this.src[ i ].reduce((r, c) => {
+			const realSrc = [ c + "_400w.jpg", c + "_700w.jpg", c + "_1200w.jpg" ].map(src => this.sources[ src ]);
+			return [ ...r, ...realSrc ];
+		}, []);
 
-		if ( "IntersectionObserver" in window && "Promise" in window )
+		if ("IntersectionObserver" in window && "Promise" in window)
 			container.innerHTML = `
 				<div class="app-slider">
 					<div class="app-image active img1">
-						<img class="central" data-src="${ slides[ 2 ] }" data-srcset="${ slides[ 0 ] } 450w,${ slides[ 1 ] } 800w, ${ slides[ 2 ] } 1200w" alt="weather-app-pic" />
+						<img class="central" data-src="${ slides[ 2 ] }" data-srcset="${ slides[ 0 ] } 400w,${ slides[ 1 ] } 700w, ${ slides[ 2 ] } 1200w" alt="weather-app-pic" />
 					</div>
 					<div class="app-image img2" >
-						<img class="right" data-src="${ slides[ 5 ] }" data-srcset="${ slides[ 3 ] } 450w,${ slides[ 4 ] } 800w, ${ slides[ 5 ] } 1200w"  alt="weather-app-pic" />
+						<img class="right" data-src="${ slides[ 5 ] }" data-srcset="${ slides[ 3 ] } 400w,${ slides[ 4 ] } 700w, ${ slides[ 5 ] } 1200w"  alt="weather-app-pic" />
 					</div>
 					<div class="app-image img3">
-						<img class="left" data-src="${ slides[ 8 ] }" data-srcset="${ slides[ 6 ] } 450w,${ slides[ 7 ] } 800w, ${ slides[ 8 ] } 1200w"  alt="weather-app-pic" />
+						<img class="left" data-src="${ slides[ 8 ] }" data-srcset="${ slides[ 6 ] } 400w,${ slides[ 7 ] } 700w, ${ slides[ 8 ] } 1200w"  alt="weather-app-pic" />
 					</div>
-				</div>`
+				</div>`;
 		else {
 			container.innerHTML = `
 				<div class="app-slider">
 					<div class="app-image active img1">
-						<img class="central" src="${ slides[ 2 ] }" srcset="${ slides[ 0 ] } 450w,${ slides[ 1 ] } 800w, ${ slides[ 2 ] } 1200w" alt="weather-app-pic" />
+						<img class="central" src="${ slides[ 2 ] }" srcset="${ slides[ 0 ] } 400w,${ slides[ 1 ] } 700w, ${ slides[ 2 ] } 1200w" alt="weather-app-pic" />
 					</div>
 					<div class="app-image img2" >
-						<img class="right" src="${ slides[ 5 ] }" srcset="${ slides[ 3 ] } 450w,${ slides[ 4 ] } 800w, ${ slides[ 5 ] } 1200w"  alt="weather-app-pic" />
+						<img class="right" src="${ slides[ 5 ] }" srcset="${ slides[ 3 ] } 400w,${ slides[ 4 ] } 700w, ${ slides[ 5 ] } 1200w"  alt="weather-app-pic" />
 					</div>
 					<div class="app-image img3">
-						<img class="left" src="${ slides[ 8 ] }" srcset="${ slides[ 6 ] } 450w,${ slides[ 7 ] } 800w, ${ slides[ 8 ] } 1200w"  alt="weather-app-pic" />
+						<img class="left" src="${ slides[ 8 ] }" srcset="${ slides[ 6 ] } 400w,${ slides[ 7 ] } 700w, ${ slides[ 8 ] } 1200w"  alt="weather-app-pic" />
 					</div>
-				</div>`
+				</div>`;
 		}
-	} )
-	this.imagesCont = [].slice.call( document.querySelectorAll( ".app-image.active" ) )
-	if ( "IntersectionObserver" in window ) {
-		this.setOnVisible()
-		this.setLazyLoad()
+	});
+	this.imagesCont = [].slice.call(document.querySelectorAll(".app-image.active"));
+	if ("IntersectionObserver" in window) {
+		this.setOnVisible();
+		this.setLazyLoad();
 	}
-}
+};
 Images.prototype.setOnVisible = function () {
-	const onVisibleObs = new IntersectionObserver( cb, { threshold: 0.01 } )
-	function cb ( entries ) {
-		entries.forEach( e => {
-			if ( e.intersectionRatio > 0 ) {
-				e.target.classList.remove( "hideRight" )
-				onVisibleObs.unobserve( e.target )
+	const onVisibleObs = new IntersectionObserver(cb, { threshold: 0.01 });
+	function cb(entries) {
+		entries.forEach(e => {
+			if (e.intersectionRatio > 0) {
+				e.target.classList.remove("hideRight");
+				onVisibleObs.unobserve(e.target);
 			}
-		} )
+		});
 	}
-	this.imagesCont.forEach( img => {
-		img.parentElement.classList.add( "hideRight" )
-		onVisibleObs.observe( img.parentElement )
-		new AutoSlide( img ).setAutoSlideObs()
-	} )
-	setIntroClasses()
-}
+	this.imagesCont.forEach(img => {
+		img.parentElement.classList.add("hideRight");
+		onVisibleObs.observe(img.parentElement);
+		new AutoSlide(img).setAutoSlideObs();
+	});
+	setIntroClasses();
+};
 
 Images.prototype.setLazyLoad = function () {
-	this.lazyObs = new IntersectionObserver( cb, { threshold: 0.01, rootMargin: "0px 0px 500px 0px" } )
-	function cb ( entries ) {
-		entries.forEach( e => {
-			if ( e.intersectionRatio > 0 ) {
-				const promise = new Promise( function ( resolve, reject ) {
-					const img = e.target.children[ 0 ]
-					img.src = img.dataset.src
-					img.srcset = img.dataset.srcset
+	this.lazyObs = new IntersectionObserver(cb, { threshold: 0.01, rootMargin: "0px 0px 500px 0px" });
+	function cb(entries) {
+		entries.forEach(e => {
+			if (e.intersectionRatio > 0) {
+				const promise = new Promise(function (resolve, reject) {
+					const img = e.target.children[ 0 ];
+					img.src = img.dataset.src;
+					img.srcset = img.dataset.srcset;
 
-					img.onload = () => resolve( e.target )
-					img.onerror = () => reject( new Error( "Could not load portfolio images" ) )
-				} )
-				promise.then( img => {
-					const next1 = img.nextElementSibling.children[ 0 ]
-					const next2 = img.nextElementSibling.nextElementSibling.children[ 0 ]
-					next1.src = next1.dataset.src
-					next1.srcset = next1.dataset.srcset
-					next2.src = next2.dataset.src
-					next2.srcset = next2.dataset.srcset
-				} )
-				this.unobserve( e.target )
+					img.onload = () => resolve(e.target);
+					img.onerror = () => reject(new Error("Could not load portfolio images"));
+				});
+				promise.then(img => {
+					const next1 = img.nextElementSibling.children[ 0 ];
+					const next2 = img.nextElementSibling.nextElementSibling.children[ 0 ];
+					next1.src = next1.dataset.src;
+					next1.srcset = next1.dataset.srcset;
+					next2.src = next2.dataset.src;
+					next2.srcset = next2.dataset.srcset;
+				});
+				this.unobserve(e.target);
 			}
-		} )
+		});
 	}
-	this.imagesCont.forEach( cont => {
-		this.lazyObs.observe( cont )
-	} )
-}
+	this.imagesCont.forEach(cont => {
+		this.lazyObs.observe(cont);
+	});
+};
 
-function AutoSlide ( center ) {
-	this.center = center.children[ 0 ]
-	this.right = this.center.parentElement.nextElementSibling.children[ 0 ]
-	this.left = this.right.parentElement.nextElementSibling.children[ 0 ]
-	this.autoSlide = ""
+function AutoSlide(center) {
+	this.center = center.children[ 0 ];
+	this.right = this.center.parentElement.nextElementSibling.children[ 0 ];
+	this.left = this.right.parentElement.nextElementSibling.children[ 0 ];
+	this.autoSlide = "";
 }
 AutoSlide.prototype.startAutoSlide = function () {
-	this.autoSlide = setInterval( autoSlideCB.bind( this ), 6500 )
+	this.autoSlide = setInterval(autoSlideCB.bind(this), 6500);
 
-	function autoSlideCB () {
-		this.center.className = this.right.className = this.left.className = "app-image"
-		this.center.classList.add( "left" )
-		this.center.parentElement.classList.remove( "active" )
-		this.right.classList.add( "center" )
-		this.right.parentElement.classList.add( "active" )
-		this.left.classList.add( "right" );
+	function autoSlideCB() {
+		this.center.className = this.right.className = this.left.className = "app-image";
+		this.center.classList.add("left");
+		this.center.parentElement.classList.remove("active");
+		this.right.classList.add("center");
+		this.right.parentElement.classList.add("active");
+		this.left.classList.add("right");
 
-		[ this.left, this.center, this.right ] = [ this.center, this.right, this.left ]
+		[ this.left, this.center, this.right ] = [ this.center, this.right, this.left ];
 	}
-}
+};
 AutoSlide.prototype.stopAutoSlide = function () {
-	clearInterval( this.autoSlide )
-}
+	clearInterval(this.autoSlide);
+};
 AutoSlide.prototype.setAutoSlideObs = function () {
-	const that = this
-	this.obs = new IntersectionObserver( cb, { threshold: 0.9, rootMargin: "0px 500px 0px 500px" } )
-	function cb ( entries ) {
-		entries.forEach( e => {
-			if ( e.intersectionRatio >= 0.9 ) {
-				that.startAutoSlide()
+	const that = this;
+	this.obs = new IntersectionObserver(cb, { threshold: 0.9, rootMargin: "0px 500px 0px 500px" });
+	function cb(entries) {
+		entries.forEach(e => {
+			if (e.intersectionRatio >= 0.9) {
+				that.startAutoSlide();
 			}
 			else {
-				that.autoSlide && that.stopAutoSlide()
+				that.autoSlide && that.stopAutoSlide();
 			}
-		} )
+		});
 	}
-	this.obs.observe( this.center.parentElement )
-}
+	this.obs.observe(this.center.parentElement);
+};
